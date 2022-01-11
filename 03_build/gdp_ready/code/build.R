51776a1b143d3d659353cddf212766888bd378ed
KS <yanyu1332@yahoo.co.jp>
2022-01-11 15:43:24 +0900

    I have done gdp_ready

--- 03_build/gdp_ready/code/build.R
+++ 03_build/gdp_ready/code/build.R
@@ -3,8 +3,20 @@ main <- function(){
   tidy_data <- read_interim(my_folder, extension = "tidy")
   
   ready_data <- tidy_data %>% 
-    ##generate GDP per capita
-    gen_gdp_per_capita()
+    ##generate GDP per capital
+    gdp_per_capital()
   
   save_interim(ready_data, my_folder, extension = "ready")
 }
+
+gdp_per_capital <- function(data_input){
+  
+  data_output <- data_input %>% 
+    dplyr::mutate(gdp_per_cap = GDP/population)
+  
+  return(data_output)
+}
+
+source("01_admin/functions/basics.R")
+
+main()
