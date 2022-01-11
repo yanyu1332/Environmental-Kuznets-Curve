f080774f7307895f1b698f7d9c22f05533e26653
KS <yanyu1332@yahoo.co.jp>
2022-01-11 15:40:30 +0900

    Have done pollution_ready

--- 03_build/pollution_ready/code/build.R
+++ 03_build/pollution_ready/code/build.R
@@ -3,39 +3,42 @@ main <- function(){
   tidy_data <- read_interim(my_folder, extension = "tidy")
   
   ready_data <- tidy_data %>% 
-    ##generate standard year table with interval = 5
-    gen_yeartab(interval = 5) 
+    ##generate data table with  year interval 5 
+    interval_data(interval = 5) 
   
   save_interim(ready_data, my_folder, extension = "ready")
 }
 
 
-gen_yeartab <- function(data_input, interval){
+interval_data <- function(data_input, interval){
   
-  data_summarized <- data_input %>% 
-    dplyr::mutate(group = round(year/interval) * 
-                    dplyr::if_else(country == "JPN", 
-                                   true = 1, 
-                                   false = 2, 
-                                   missing = 3)) %>% 
-    dplyr::group_by(group) %>% 
+  data_input <- tidy_data #test用
+  interval <- 5
+  
+  ## 1. ５年おきのデータ(例: 1960, 1965, 1970, ...) のみを抽出しなさい
+  data_output <- data_input %>%
+    dplyr::filter(year %% interval == 0) %>% 
+    dplyr::select(-c(pollution_original))
+  
+  ## 2. ５年の間の年を、より近い年に含めて、平均値を計算しなさい
+  data_summarized <- data_input %>%
+    ##interval 年を基準に年を割り振る
+    dplyr::mutate(group = round(year/interval) *
+                    dplyr::if_else(country == "JPN",
+                                   true = 1,
+                                   false = 2,
+                                   missing = 3)) %>%
+    dplyr::mutate(every_5_years = group * interval )%>% 
+    dplyr::group_by(every_5_years,country) %>%
     dplyr::summarise(pollution = mean(pollution_numeric, na.rm = TRUE),
-                     missing_rate = mean(missing_dummy)) %>% 
-    dplyr::ungroup() 
-  
-  data_output <- data_input %>% 
-    dplyr::mutate(quotient = year/interval) %>% 
-    dplyr::mutate(group = round(quotient) * 
-                    dplyr::if_else(country == "JPN", 
-                                   true = 1, 
-                                   false = 2, 
-                                   missing = 3)) %>% 
-    dplyr::filter(quotient %% 1 == 0) %>% 
-    dplyr::select(-pollution_numeric) %>% 
-    dplyr::left_join(data_summarized, by = ("group" = "group")) %>% 
-    dplyr::select(-c("group", "quotient","pollution_original","missing_dummy")) %>% 
-    dplyr::rename(group_5year = year)
-  
+                     missing_rate = mean(missing_dummy)) %>%
+    dplyr::ungroup() %>% 
+    dplyr::mutate(years = every_5_years / dplyr::if_else(country == "JPN",
+                                                         true = 1,
+                                                         false = 2,
+                                                         missing = 3)) %>% 
+    dplyr::select(-c(every_5_years))
+    
   return(data_output)  
 }
 
