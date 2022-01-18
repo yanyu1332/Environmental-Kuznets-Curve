3006f65cc0eb52ee32cb30f44f92e8535ae20978
KS <yanyu1332@yahoo.co.jp>
2022-01-18 18:49:40 +0900

    (b)クズネッツ曲線の作成完了

--- 04_analyze/initial/code/analyze.R
+++ 04_analyze/initial/code/analyze.R
@@ -1,7 +1,42 @@
 main <- function(){
-  box::use(`functions`/basics)
+  my_folder <- "initial"
+  ##データの読み込み
+  data_master <- read_interim("master")
+  main_varnames <- c()
+  output_folder <- "initial"
+  
+  
+
 
 }
 
+## ==============================
+# (a) 記述統計の作成
+## (1)度数分布表の作成
+gen_quantile <- function(data){
+  
+}
+## (2)代表値の作成(mean,median,variance)
+gen_central_tendency <- function(){
+  
+}
+
+## ==============================
+# (b) 図の作成
+# 環境クズネッツ曲線の仮説を視覚的に検証するための図を作成しなさい
+# 他者と共有できるように図を整理しなさい
+
+plot_kuznets_curve <-
+
+## ==============================
+# (c) 回帰分析
+# 回帰分析で環境クズネッツ曲線の仮説を検証するには、どのような回帰式を推定すればよいですか
+# 回帰分析の結果を表としてまとめ、議論しなさい
+
+plot_regression <- function(){
+  
+}
+
+## ==============================
 
 main()
